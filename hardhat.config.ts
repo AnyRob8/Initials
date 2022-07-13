5b47504aa140b03c552a6f8877d49bbd2bf0b868
Veljko Kovacevic <veljko.kov@hotmail.com>
2022-07-13 13:38:41 +0200

    add abi

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-
+import "@ethersproject/abi";
+import "hardhat-abi-exporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.15",
@@ -21,6 +22,11 @@ const config: HardhatUserConfig = {
       rinkeby: process.env.ETHERSCAN_KEY ?? "",
       mainnet: process.env.ETHERSCAN_KEY ?? "",
     }
+  },
+  abiExporter: {
+    path: './abis',
+    clear: true,
+    flat: true
   }
 };
 
